cc3a882d86de429f3482db4a193bdf8016be8eec
AedesAejyptis <anjoslucas7@gmail.com>
2017-05-13 20:54:58 -0400

    Click no login :+1:
    Vai lá ver kk

--- VamosLaVer/_Javascript/contact-us-js.js
+++ VamosLaVer/_Javascript/contact-us-js.js
@@ -1,25 +1,55 @@
-// Get the modal
-var modal = document.getElementById('modal-rate-this');
+ // Get the modal
+function gomodelsidebar(){
 
-// Get the button that opens the modal
-var btn = document.getElementById("btn-sidenav");
+            var modal = document.getElementById('modal-rate-this');
+            
+            // Get the button that opens the modal
+            var a = document.getElementById("btn-sidenav");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+            // When the user clicks the button, open the modal 
+            a.onclick = function() {
+                modal.style.display = "block";
+            }
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
 }
+function gomodellogin(){
+    // Get the modal in login
+            var modall = document.getElementById('modal-login');
+            
+            // Get the button that opens the modal login
+            var al = document.getElementById("a-model-login");
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+            // Get the <span> element that closes the modal login
+            var spanl = document.getElementsByClassName("closel")[0];
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+            // When the user clicks the button, open the modal login
+            al.onclick = function() {
+                modall.style.display = "block";
+            }
+
+            // When the user clicks on <span> (x), close the modal login
+            spanl.onclick = function() {
+                modall.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modall) {
+                    modall.style.display = "none";
+                }
+            }
 }
